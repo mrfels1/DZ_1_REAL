ef8c4e82591995d6f8bc052397f3953000c17bb6
mrfels1 <58826166+mrfels1@users.noreply.github.com>
2023-10-02 19:12:54 +0300

    fin

--- src/Ball.java
+++ src/Ball.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class ball {
+public class Ball {
+    public String getColor() {
+        return color;
+    }public void setColor(String color) {
+        this.color = color;
+    }private String color; private double radius;
+    public Ball(String color, double radius) {
+        this.color = color;
+        this.radius = radius;
+    }public double getRadius() {
+        return radius;
+    }public void setRadius(double radius) {
+        this.radius = radius;
+    }public double calculateVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+    }public String toString() {
+        return "A " + color + " ball with a radius of " + radius + " units.";
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("----- Dog Class Testing -----");
+        Dog d1 = new Dog("Mike", 2);
+        Dog d2 = new Dog("Helen", 7);
+        Dog d3 = new Dog("Bob"); d3.setAge(1);
+        System.out.println(d1);
+        d1.intoHumanAge();
+        d2.intoHumanAge();
+        d3.intoHumanAge();
+
+        // Ball Testing
+        System.out.println("\n----- Ball Class Testing -----");
+
+        Ball ball1 = new Ball("Red", 5.0);
+        Ball ball2 = new Ball("Blue", 3.5);
+
+        System.out.println(ball1);
+        System.out.println(ball2);
+
+        ball1.setColor("Green");
+        ball2.setRadius(4.0);
+
+        System.out.println("Updated information:");
+        System.out.println(ball1);
+        System.out.println(ball2);
+
+        System.out.println("Volume of ball1: " + ball1.calculateVolume());
+        System.out.println("Volume of ball2: " + ball2.calculateVolume());
+
+
+        // Book Testing
+        System.out.println("\n----- Book Class Testing -----");
+
+        Book book1 = new Book("Java Programming", "John Doe", 400);
+        Book book2 = new Book("Python for Beginners", "Jane Smith", 250);
+
+        System.out.println(book1);
+        System.out.println(book2);
+
+        book1.setTitle("Advanced Java Programming");
+        book2.setAuthor("John Smith");
+        book2.setPages(300);
+
+        System.out.println("Updated information:");
+        System.out.println(book1);
+        System.out.println(book2);
     }
 }
